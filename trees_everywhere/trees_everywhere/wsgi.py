8420e31574a6eaa6056d134d6057970dd0715655
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2024-07-01 15:27:37 -0400

    adicionando configs de deploy

--- trees_everywhere/trees_everywhere/wsgi.py
+++ trees_everywhere/trees_everywhere/wsgi.py
@@ -5,6 +5,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trees_everywhere.settings')
 
-application = get_wsgi_application()
-
-app = application
+app = get_wsgi_application()
