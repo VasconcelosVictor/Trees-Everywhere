2708b0d04c2ffa158fd86adba05139c6ed206527
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2024-07-01 14:55:44 -0400

    correcoes pra deploy

--- trees_everywhere/trees_everywhere/settings.py
+++ trees_everywhere/trees_everywhere/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-d28kgj1jace&65w_0z01+g+lq1z$sy^@93t*z1#0hjbots)bvm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 TESTING = True
 
